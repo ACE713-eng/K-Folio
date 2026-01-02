98ee64bbf2a5fe94f37e7f0bc94c513300024013
tishyajena <tishyajena719@gmail.com>
2026-01-02 22:33:16 +0530

    Add new background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
 import SignIn from "./components/SignIn"
+import Background from "./Component/Background"
 function App() {
 
   return (
     <>
+    <Background/>
     <SignIn />
     </>
   )
---
+++ frontend/src/Component/Background.tsx
@@ -0,0 +1,26 @@
+
+import MainContent from "./MainContent";
+
+// React.FC provides the type for a Functional Component
+const Background: React.FC = () => {
+  return (
+    <div className="bg-[linear-gradient(140deg,#020B3A_0%,#01030A_30%,#01030A_40%,#020B3A_90%,#010B6A_5%)] relative min-h-screen w-screen flex">
+      <div
+        className="absolute top-0 left-0 h-screen w-7/12 bg-repeat bg-left bg-contain opacity-65"
+        style={{
+          backgroundImage: "url('/bg-pattern.svg')",
+        }}
+      />
+
+      <div className="relative z-10 h-screen w-7/12 flex">
+        <MainContent />
+      </div>
+
+      <div className="w-5/12 bg-black">
+        <h1 className="text-amber-50"></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
